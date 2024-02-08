d13666d9d5273c2289dfefaf70a533c428ff6a18
Anthony Griffon <anthony@griffon.one>
2024-02-08 14:55:47 +0100

    feat: add wasi_ext
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -19,6 +19,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![cfg_attr(loom, allow(dead_code, unreachable_pub))]
+#![cfg_attr(all(target_arch = "wasm32", target_os = "wasi"), feature(wasi_ext))]
 
 //! A runtime for writing reliable network applications without compromising speed.
 //!
